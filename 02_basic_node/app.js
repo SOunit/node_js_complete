b01452ad8325e6242517a8d024d6b7e9d8033c77
SOunit <workbookwork7@gmail.com>
2021-05-14 08:54:38 +0900

    add response

--- 02_basic_node/app.js
+++ 02_basic_node/app.js
@@ -3,5 +3,18 @@ const server = http.createServer((req, res) => {
   console.log('URL: ', req.url);
   console.log('Method: ', req.method);
   console.log('Headers: ', req.headers);
+
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<header>');
+  res.write('<title>Node Page');
+  res.write('</title>');
+  res.write('</header>');
+  res.write('<body>');
+  res.write('<h1>Node Page');
+  res.write('</h1>');
+  res.write('</body>');
+  res.write('</html>');
+  res.end();
 });
 server.listen(3000);
