0d8e44bea5aa68bc7162ec826dda8f4055fa4c27
SOunit <workbookwork7@gmail.com>
2021-05-14 09:06:24 +0900

    add input page

--- 02_basic_node/app.js
+++ 02_basic_node/app.js
@@ -4,6 +4,23 @@ const server = http.createServer((req, res) => {
   console.log('Method: ', req.method);
   console.log('Headers: ', req.headers);
 
+  if (req.url === '/') {
+    res.write('<html>');
+    res.write('<header>');
+    res.write('<title>Input Page');
+    res.write('</title>');
+    res.write('</header>');
+    res.write('<body>');
+    res.write('<form action="/message" method="POST">');
+    res.write('<input name="message">');
+    res.write('<button>Submit');
+    res.write('</button>');
+    res.write('</form>');
+    res.write('</body>');
+    res.write('</html>');
+    return res.end();
+  }
+
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<header>');
