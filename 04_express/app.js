c36d56f58f91f8e0d4a528b2c9b908f6acd98de0
SOunit <workbookwork7@gmail.com>
2021-05-18 07:27:07 +0900

    add routes handler

--- 04_express/app.js
+++ 04_express/app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('in 1st middleware');
+app.use('/', (req, res, next) => {
+  console.log('this always run');
   next();
 });
 
-app.use((req, res, next) => {
+app.use('/add-products', (req, res, next) => {
+  console.log('in 1st middleware');
+  res.send('<h1>Add Product</h1>');
+});
+
+app.use('/', (req, res, next) => {
   console.log('in 2nd middleware');
   res.send('<h1>test</h1>');
 });
