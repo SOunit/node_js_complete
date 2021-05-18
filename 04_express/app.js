aaaf084a1920ff1fdc6dc5b90b974fb648456eac
SOunit <workbookwork7@gmail.com>
2021-05-18 13:09:20 +0900

    add css

--- 04_express/app.js
+++ 04_express/app.js
@@ -7,9 +7,14 @@ const shopRouter = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// router
 app.use('/admin', adminRouter);
 app.use(shopRouter);
 
+// to import css
+app.use(express.static(path.join(__dirname, 'public')));
+
+// 404 router
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
