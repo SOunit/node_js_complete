4d3799797fe314662b29960ebd7f4004cb9caf49
SOunit <workbookwork7@gmail.com>
2021-05-20 08:57:28 +0900

    add 404 controller

--- 07_mvc/app.js
+++ 07_mvc/app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const adminRoutes = require('./routes/admin');
 const shopRouter = require('./routes/shop');
+const errorController = require('./controllers/error');
 
 const app = express();
 
@@ -20,12 +21,6 @@ app.use(shopRouter);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // 404 router
-app.use((req, res, next) => {
-  res.status(404).render('404', {
-    pageTitle: 'Page Not Found',
-    productCSS: false,
-    formsCSS: false,
-  });
-});
+app.use(errorController.get404);
 
 app.listen(3000);
