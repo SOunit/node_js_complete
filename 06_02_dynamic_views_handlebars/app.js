13c8c2ff001817fc38ffcd6e4c68d082d96b83d4
SOunit <workbookwork7@gmail.com>
2021-05-19 20:54:03 +0900

    add hanblebars view samples

--- 06_02_dynamic_views_handlebars/app.js
+++ 06_02_dynamic_views_handlebars/app.js
@@ -10,7 +10,14 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // template engine
-app.engine('hbs', expressHbs());
+app.engine(
+  'hbs',
+  expressHbs({
+    layoutsDir: 'views/layouts/',
+    defaultLayout: 'main-layout',
+    extname: 'hbs',
+  })
+);
 app.set('view engine', 'hbs');
 app.set('views', 'views');
 
