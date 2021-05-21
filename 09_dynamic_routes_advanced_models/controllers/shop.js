54550cb0efeb16c1c95dc926e1f4f354da8a1ac7
SOunit <workbookwork7@gmail.com>
2021-05-21 13:14:37 +0900

    add new project for lesson 09

---
+++ 09_dynamic_routes_advanced_models/controllers/shop.js
@@ -0,0 +1,55 @@
+const Product = require('../models/product');
+
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render('shop/index', {
+      path: '/',
+      productCSS: true,
+      formsCSS: false,
+      pageTitle: 'Shop',
+      prods: products,
+    });
+  });
+};
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render('shop/product-list', {
+      prods: products,
+      pageTitle: 'All Products',
+      path: '/products',
+      productCSS: true,
+      formsCSS: false,
+    });
+  });
+};
+
+exports.getCart = (req, res, next) => {
+  res.render('shop/cart', {
+    path: '/cart',
+    productCSS: true,
+    formsCSS: false,
+    pageTitle: 'Your Cart',
+    prods: [],
+  });
+};
+
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    path: '/orders',
+    productCSS: true,
+    formsCSS: false,
+    pageTitle: 'Your Order',
+    prods: [],
+  });
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render('shop/checkout', {
+    path: '/checkout',
+    productCSS: true,
+    formsCSS: false,
+    pageTitle: 'Checkout',
+    prods: [],
+  });
+};
---
+++ 09_dynamic_routes_advanced_models/models/product.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const path = require('path');
+
+// create products json path
+const p = path.join(
+  path.dirname(require.main.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = (cb) => {
+  // try to get data from json
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      // err means
+      // data NOT exists in json file
+      cb([]);
+    } else {
+      // data exists in json file
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
+module.exports = class Product {
+  constructor(title, imageUrl, price, description) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.price = price;
+    this.description = description;
+  }
+
+  save() {
+    getProductsFromFile((products) => {
+      // add new product
+      products.push(this);
+
+      // save products to json file
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};
