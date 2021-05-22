e405475a862b5e809bfe5dc27782777ab14c921a
SOunit <workbookwork7@gmail.com>
2021-05-22 10:41:08 +0900

    add edit product page

--- 09_dynamic_routes_advanced_models/controllers/admin.js
+++ 09_dynamic_routes_advanced_models/controllers/admin.js
@@ -1,11 +1,12 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
+  res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
     productCSS: true,
     formsCSS: true,
+    editing: false,
   });
 };
 
@@ -19,6 +20,33 @@ exports.postAddProduct = (req, res, next) => {
   product.save();
   res.redirect('/');
 };
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+
+  if (!editMode) {
+    // return to stop process here
+    return res.redirect('/');
+  }
+
+  const prodId = req.params.productId;
+  Product.findById(prodId, (product) => {
+    if (!product) {
+      return res.redirect('/');
+    }
+
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      productCSS: true,
+      formsCSS: true,
+      editing: editMode,
+      product,
+    });
+  });
+};
+
+exports.postEditProduct = (req, res, next) => {};
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
