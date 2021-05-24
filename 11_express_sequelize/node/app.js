1fa66ddb8a3cf8f9c9b89d4b29f371d2067dadaa
SOunit <workbookwork7@gmail.com>
2021-05-24 21:44:15 +0900

    add cart and cart-item models

--- 11_express_sequelize/node/app.js
+++ 11_express_sequelize/node/app.js
@@ -9,6 +9,8 @@ const shopRouter = require('./routes/shop');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const app = express();
 
@@ -39,9 +41,13 @@ app.use(errorController.get404);
 // create relation
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 // create tables
-const reCreateTable = false;
+const reCreateTable = true;
 sequelize
   .sync({ force: reCreateTable })
   .then((result) => {
