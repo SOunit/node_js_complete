993041cfc7f80e033836aed44ebe4b4ea5fa8819
SOunit <workbookwork7@gmail.com>
2021-05-24 21:25:05 +0900

    fetch data related to current user

--- 11_express_sequelize/node/controllers/admin.js
+++ 11_express_sequelize/node/controllers/admin.js
@@ -38,8 +38,10 @@ exports.getEditProduct = (req, res, next) => {
   }
 
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
-    .then((product) => {
+  req.user
+    .getProducts({ where: { id: prodId } })
+    .then((products) => {
+      const product = products[0];
       if (!product) {
         return res.redirect('/');
       }
@@ -77,7 +79,8 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user
+    .getProducts()
     .then((products) => {
       res.render('admin/products', {
         pageTitle: 'Admin Products',
