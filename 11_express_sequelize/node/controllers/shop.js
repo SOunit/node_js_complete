ede656dcc52925e32d53d6d5a6c1e41eec24e0ad
SOunit <workbookwork7@gmail.com>
2021-05-25 13:12:06 +0900

    add creating order

--- 11_express_sequelize/node/controllers/shop.js
+++ 11_express_sequelize/node/controllers/shop.js
@@ -1,5 +1,4 @@
 const Product = require('../models/product');
-const Cart = require('../models/cart');
 
 exports.getIndex = (req, res, next) => {
   Product.findAll()
@@ -109,6 +108,33 @@ exports.postCart = (req, res, next) => {
     .catch((err) => {
       console.log(err);
     });
+};
+
+exports.postOrder = (req, res, next) => {
+  req.user
+    .getCart()
+    .then((cart) => {
+      return cart.getProducts();
+    })
+    .then((products) => {
+      return req.user
+        .createOrder()
+        .then((order) => {
+          order.addProducts(
+            products.map((product) => {
+              product.orderItem = { quantity: product.cartItem.quantity };
+              return product;
+            })
+          );
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    .then((result) => {
+      res.redirect('/orders');
+    })
+    .catch((err) => console(err));
 };
 
 exports.getOrders = (req, res, next) => {
--- 11_express_sequelize/node/routes/shop.js
+++ 11_express_sequelize/node/routes/shop.js
@@ -13,6 +13,8 @@ router.post('/cart', shopController.postCart);
 router.get('/checkout', shopController.getCheckout);
 router.get('/orders', shopController.getOrders);
 
+router.post('/create-order', shopController.postOrder);
+
 router.post('/cart-delete-item', shopController.postCartDeleteProduct);
 
 module.exports = router;
