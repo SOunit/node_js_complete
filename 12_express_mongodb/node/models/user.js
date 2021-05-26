9e4fd4fbede7b06ab4411e23e50e71df696bb0c8
SOunit <workbookwork7@gmail.com>
2021-05-26 20:43:25 +0900

    add cart to user

--- 12_express_mongodb/node/models/user.js
+++ 12_express_mongodb/node/models/user.js
@@ -2,15 +2,27 @@ const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class User {
-  constructor(userName, email, id) {
+  constructor(userName, email, id, cart) {
     this.name = userName;
     this.email = email;
-    this.id = id ? mongodb.ObjectId(id) : null;
+    this.cart = cart; // {items: []}
+    this._id = id ? mongodb.ObjectId(id) : null;
   }
 
   save = () => {
     const db = getDb();
     return db.collection('users').insertOne(this);
+  };
+
+  addToCart = (product) => {
+    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    const db = getDb();
+    return db
+      .collection('users')
+      .updateOne(
+        { _id: mongodb.ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      );
   };
 
   static findById = (userId) => {
