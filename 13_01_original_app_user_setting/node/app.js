fc22059661be0c7f625e96810bcedc86b4460ec6
SOunit <workbookwork7@gmail.com>
2021-05-28 20:35:29 +0900

    add original user setting in app

---
+++ 13_01_original_app_user_setting/node/app.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const errorController = require('./controllers/error');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const User = require('./models/user');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// template engine
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+// to import css
+app.use(express.static(path.join(__dirname, 'public')));
+
+// set user
+app.use((req, res, next) => {
+  // fetch user
+  User.findById('60b0225c202db80324c80e6a')
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
+// router
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(errorController.get404);
+
+mongoose
+  .connect('mongodb://mongo:27017/shop')
+  .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: 'Max',
+          email: 'max@test.com',
+          cart: {
+            items: [],
+          },
+        });
+        user.save();
+      }
+    });
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
