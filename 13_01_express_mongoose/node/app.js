83088c36b30f1358b89334804aec4ef7aebb5272
SOunit <workbookwork7@gmail.com>
2021-05-29 14:11:55 +0900

    add renamed project for lecture 13

---
+++ 13_01_express_mongoose/node/app.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const errorController = require('./controllers/error');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const User = require('./models/user');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// template engine
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+// to import css
+app.use(express.static(path.join(__dirname, 'public')));
+
+// set user
+app.use((req, res, next) => {
+  // try to fetch user
+  User.findOne()
+    .then((user) => {
+      // if user NOT exist
+      if (!user) {
+        const user = new User({
+          name: 'Max',
+          email: 'max@test.com',
+          cart: {
+            items: [],
+          },
+        });
+        return user.save();
+      }
+      // if user exist
+      return user;
+    })
+    .then((user) => {
+      console.log(user);
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
+// router
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(errorController.get404);
+
+mongoose
+  .connect('mongodb://mongo:27017/shop')
+  .then((result) => {
+    // console.log(result);
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
