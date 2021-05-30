ae7b5b582d5c172c859f7a7929b94960ffdd4545
SOunit <workbookwork7@gmail.com>
2021-05-30 16:21:31 +0900

    add enctypting password for user

--- 15_user_auth/node/controllers/auth.js
+++ 15_user_auth/node/controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const bcrypt = require('bcryptjs');
 
 exports.getLogin = (req, res, next) => {
   console.log(req.session.isLoggedIn);
@@ -58,15 +59,19 @@ exports.postSignup = (req, res, next) => {
         return res.redirect('/signup');
       }
 
-      const user = new User({
-        email,
-        password,
-        cart: { items: [] },
-      });
-      return user.save();
-    })
-    .then((result) => {
-      res.redirect('/login');
+      return bcrypt
+        .hash(password, 12)
+        .then((hashedPassword) => {
+          const user = new User({
+            email,
+            password: hashedPassword,
+            cart: { items: [] },
+          });
+          return user.save();
+        })
+        .then((result) => {
+          res.redirect('/login');
+        });
     })
     .catch((err) => console.log(err));
 };
