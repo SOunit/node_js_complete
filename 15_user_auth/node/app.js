92a08fe902039396e2ce2da60affec613f0c8edb
SOunit <workbookwork7@gmail.com>
2021-05-30 23:14:34 +0900

    add message for login error

--- 15_user_auth/node/app.js
+++ 15_user_auth/node/app.js
@@ -1,3 +1,4 @@
+// import packages
 const path = require('path');
 const bodyParser = require('body-parser');
 const express = require('express');
@@ -5,16 +6,21 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
+const flash = require('connect-flash');
 
+// import classes
 const errorController = require('./controllers/error');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
+// import classes for mongodb
 const User = require('./models/user');
 
+// create constant
 const MONGO_DB_URL = 'mongodb://mongo:27017/shop';
 
+// setup variables for app
 const app = express();
 const store = new MongoDBStore({
   uri: MONGO_DB_URL,
@@ -22,7 +28,7 @@ const store = new MongoDBStore({
 });
 const csrfProtection = csrf();
 
-// middle wares
+// setup middle wares
 app.use(bodyParser.urlencoded({ extended: false }));
 // template engine
 app.set('view engine', 'ejs');
@@ -40,6 +46,8 @@ app.use(
 );
 // csurf needs session, so this must be after session setting code
 app.use(csrfProtection);
+// to show error messages and etc. for user experience
+app.use(flash());
 
 // setup user with mongoose method, save it in request
 app.use((req, res, next) => {
