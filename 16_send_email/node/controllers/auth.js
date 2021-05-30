3247a88f1a8758b6d82d18a192051256083aa82c
SOunit <workbookwork7@gmail.com>
2021-05-30 23:45:40 +0900

    add new project for lesson 16

---
+++ 16_send_email/node/controllers/auth.js
@@ -0,0 +1,109 @@
+const User = require('../models/user');
+const bcrypt = require('bcryptjs');
+
+exports.getLogin = (req, res, next) => {
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    errorMessage: message,
+    productCSS: false,
+    formsCSS: true,
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // try to fetch user
+  // if email and password match, save data to session
+  // else redirect to login page
+  User.findOne({ email: email })
+    .then((user) => {
+      if (!user) {
+        req.flash('error', 'Invalid email or password.');
+        return res.redirect('/login');
+      }
+      bcrypt.compare(password, user.password).then((doMatch) => {
+        if (doMatch) {
+          // save data to mongodb session collection
+          req.session.isLoggedIn = true;
+          // user lost mongoose methods when saved in mongodb
+          // so in app.js re-fetching object is needed by using User model
+          req.session.user = user;
+
+          // use save mthod to prevent redirecting before mongodb update finish
+          return req.session.save((err) => {
+            console.log(err);
+            res.redirect('/');
+          });
+        }
+        req.flash('error', 'Invalid email or password.');
+        res.redirect('/login');
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy((err) => {
+    console.log(err);
+    res.redirect('/');
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    errorMessage: message,
+    productCSS: false,
+    formsCSS: true,
+  });
+};
+
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+
+  User.findOne({ email: email })
+    .then((userDoc) => {
+      if (userDoc) {
+        req.flash(
+          'error',
+          'E-Mail exists already. Please pick a different one.'
+        );
+        return res.redirect('/signup');
+      }
+
+      return bcrypt
+        .hash(password, 12)
+        .then((hashedPassword) => {
+          const user = new User({
+            email,
+            password: hashedPassword,
+            cart: { items: [] },
+          });
+          return user.save();
+        })
+        .then((result) => {
+          res.redirect('/login');
+        });
+    })
+    .catch((err) => console.log(err));
+};
