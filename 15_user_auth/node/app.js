5a95572b48e5d470ea20127ed7f37750e5a4b3c3
SOunit <workbookwork7@gmail.com>
2021-05-30 14:23:59 +0900

    add new project for lesson 15

---
+++ 15_user_auth/node/app.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+
+const errorController = require('./controllers/error');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
+
+const User = require('./models/user');
+
+const MONGO_DB_URL = 'mongodb://mongo:27017/shop';
+
+const app = express();
+const store = new MongoDBStore({
+  uri: MONGO_DB_URL,
+  collection: 'sessions',
+});
+
+// middle wares
+app.use(bodyParser.urlencoded({ extended: false }));
+// template engine
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+// to import css
+app.use(express.static(path.join(__dirname, 'public')));
+// session config
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
+
+// setup user with mongoose method, save it in request
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+
+  User.findById(req.session.user._id)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
+// router
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+app.use(errorController.get404);
+
+mongoose
+  .connect(MONGO_DB_URL)
+  .then((result) => {
+    // console.log(result);
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
---
+++ 15_user_auth/node/controllers/auth.js
@@ -0,0 +1,53 @@
+const User = require('../models/user');
+
+exports.getLogin = (req, res, next) => {
+  console.log(req.session.isLoggedIn);
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: req.session.isLoggedIn,
+    productCSS: false,
+    formsCSS: true,
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  // try to fetch user
+  User.findOne()
+    .then((user) => {
+      // if user NOT exist
+      if (!user) {
+        const user = new User({
+          name: 'Max',
+          email: 'max@test.com',
+          cart: {
+            items: [],
+          },
+        });
+        return user.save();
+      }
+      // if user exist
+      return user;
+    })
+    .then((user) => {
+      // save data to mongodb session collection
+      req.session.isLoggedIn = true;
+      // user lost mongoose methods when saved in mongodb
+      // so in app.js re-fetching object is needed by using User model
+      req.session.user = user;
+
+      // use save mthod to prevent redirecting before mongodb update finish
+      req.session.save((err) => {
+        console.log(err);
+        res.redirect('/');
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy((err) => {
+    console.log(err);
+    res.redirect('/');
+  });
+};
