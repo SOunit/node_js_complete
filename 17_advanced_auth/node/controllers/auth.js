81266ec0d3f4d94fe8ad89d37022ef516291da52
SOunit <workbookwork7@gmail.com>
2021-05-31 22:45:28 +0900

    add updating password logic

--- 17_advanced_auth/node/controllers/auth.js
+++ 17_advanced_auth/node/controllers/auth.js
@@ -4,6 +4,7 @@ const nodemailer = require('nodemailer');
 
 const envs = require('../envs');
 const User = require('../models/user');
+const user = require('../models/user');
 const transporter = nodemailer.createTransport({
   host: 'smtp.mailtrap.io',
   port: 2525,
@@ -201,9 +202,37 @@ exports.getNewPassword = (req, res, next) => {
         pageTitle: 'New Password',
         errorMessage: message,
         userId: user._id.toString(),
+        passwordToken: token,
         productCSS: false,
         formsCSS: true,
       });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postNewPassword = (req, res, next) => {
+  const newPassword = req.body.password;
+  const userId = req.body.userId;
+  const passwordToken = req.body.passwordToken;
+  let resetUser;
+
+  User.findOne({
+    resetToken: passwordToken,
+    resetTokenExpiration: { $gt: Date.now() },
+    _id: userId,
+  })
+    .then((user) => {
+      resetUser = user;
+      return bcrypt.hash(newPassword, 12);
+    })
+    .then((hashedPassword) => {
+      resetUser.password = hashedPassword;
+      resetUser.resetToken = null;
+      resetUser.resetTokenExpiration = null;
+      return resetUser.save();
+    })
+    .then((result) => {
+      res.redirect('/login');
     })
     .catch((err) => console.log(err));
 };
