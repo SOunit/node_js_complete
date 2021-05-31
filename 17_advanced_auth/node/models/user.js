dde0f0b1290873848165bb2e24a2010ce58f4501
SOunit <workbookwork7@gmail.com>
2021-05-31 13:03:12 +0900

    add new project for lesson 17

---
+++ 17_advanced_auth/node/models/user.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  cart: {
+    items: [
+      {
+        productId: {
+          type: Schema.Types.ObjectId,
+          ref: 'Product',
+          required: true,
+        },
+        quantity: { type: Number, required: true },
+      },
+    ],
+  },
+});
+
+// this here needs function syntacs for "this" reference
+userSchema.methods.addToCart = function (product) {
+  // try to fetch product from cart
+  const cartProductIndex = this.cart.items.findIndex((cp) => {
+    return cp.productId.toString() === product._id.toString();
+  });
+
+  // setup variables
+  let newQuantity = 1;
+  const updatedCartItems = [...this.cart.items];
+
+  // if cart has item
+  if (cartProductIndex >= 0) {
+    // update product by adding quantity
+    newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+    updatedCartItems[cartProductIndex].quantity = newQuantity;
+  } else {
+    // add new product to cart
+    updatedCartItems.push({
+      productId: product._id,
+      quantity: newQuantity,
+    });
+  }
+  const updatedCart = {
+    items: updatedCartItems,
+  };
+
+  this.cart = updatedCart;
+  return this.save();
+};
+
+userSchema.methods.removeFromCart = function (productId) {
+  const updatedCartItems = this.cart.items.filter((item) => {
+    return item.productId.toString() !== productId.toString();
+  });
+  this.cart.items = updatedCartItems;
+  return this.save();
+};
+
+userSchema.methods.clearCart = function () {
+  this.cart = { items: [] };
+  this.save();
+};
+
+module.exports = mongoose.model('User', userSchema);
