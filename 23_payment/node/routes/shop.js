09ee56e779dd3cd0eb45cca4c369bac57d2dfcc1
SOunit <workbookwork7@gmail.com>
2021-06-05 22:11:34 +0900

    add checkout page

--- 23_payment/node/routes/shop.js
+++ 23_payment/node/routes/shop.js
@@ -12,6 +12,8 @@ router.get('/products/:productId', shopController.getProduct);
 router.get('/cart', isAuth, shopController.getCart);
 router.post('/cart', isAuth, shopController.postCart);
 
+router.get('/checkout', isAuth, shopController.getCheckout);
+
 router.post('/create-order', isAuth, shopController.postOrder);
 router.get('/orders', isAuth, shopController.getOrders);
 
