3efcf217ce2645eab64aba2dc6abb1266ca9e5a3
SOunit <workbookwork7@gmail.com>
2021-06-05 14:55:44 +0900

    add new project for lesson 21

---
+++ 21_pagination/node/controllers/error.js
@@ -0,0 +1,19 @@
+exports.get404 = (req, res, next) => {
+  res.status(404).render('404', {
+    path: '/404',
+    pageTitle: 'Page Not Found',
+    isAuthenticated: req.session.isLoggedIn,
+    productCSS: false,
+    formsCSS: false,
+  });
+};
+
+exports.get500 = (req, res, next) => {
+  res.status(500).render('500', {
+    path: '/500',
+    pageTitle: 'Error!',
+    isAuthenticated: req.session.isLoggedIn,
+    productCSS: false,
+    formsCSS: false,
+  });
+};
