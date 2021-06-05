b357ad814ed597352dd6ef5232d7ac86b0e11d2e
SOunit <workbookwork7@gmail.com>
2021-06-05 16:23:21 +0900

    fetch data for a page

--- 21_pagination/node/controllers/shop.js
+++ 21_pagination/node/controllers/shop.js
@@ -5,8 +5,16 @@ const PDFDocument = require('pdfkit');
 const Product = require('../models/product');
 const Order = require('../models/order');
 
+const ITEMS_PER_PAGE = 2;
+
 exports.getIndex = (req, res, next) => {
+  // get query created by node.js.
+  // query is like, /?page=1
+  const page = req.query.page;
+
   Product.find()
+    .skip((page - 1) * ITEMS_PER_PAGE)
+    .limit(ITEMS_PER_PAGE)
     .then((products) => {
       res.render('shop/index', {
         path: '/',
