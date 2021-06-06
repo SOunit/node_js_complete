3b1690e3f961b186cef4191d6d419fa37400d2bd
SOunit <workbookwork7@gmail.com>
2021-06-06 19:13:06 +0900

    fetch posts from rest api

--- 25_rest_api/node/controllers/feed.js
+++ 25_rest_api/node/controllers/feed.js
@@ -1,7 +1,18 @@
 exports.getPosts = (req, res, next) => {
-  res
-    .status(200)
-    .json({ title: 'First post', content: 'This is the first post!' });
+  res.status(200).json({
+    posts: [
+      {
+        _id: '1',
+        title: 'First Post',
+        content: 'This is the first post!',
+        imageUrl: 'images/che_prof.jpeg',
+        creator: {
+          name: 'Jack',
+        },
+        createdAt: new Date(),
+      },
+    ],
+  });
 };
 
 exports.createPost = (req, res, next) => {
