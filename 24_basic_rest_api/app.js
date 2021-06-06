2f2031989c567ff235a3bf52f4cb9256a56feae2
SOunit <workbookwork7@gmail.com>
2021-06-06 14:58:11 +0900

    add post request in rest api

--- 24_basic_rest_api/app.js
+++ 24_basic_rest_api/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
+
+// for x-www-form-urlencode, for data in form request
+// app.use(bodyParser.json());
+
+// for application/json, for json data in request
+app.use(bodyParser.json());
 
 // routes
 app.use('/feed', feedRoutes);
