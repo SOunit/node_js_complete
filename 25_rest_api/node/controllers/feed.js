a388d8a2742a5871c531c5026bdbeb323b3ac6c2
SOunit <workbookwork7@gmail.com>
2021-06-06 19:28:14 +0900

    add posting data to rest api

--- 25_rest_api/node/controllers/feed.js
+++ 25_rest_api/node/controllers/feed.js
@@ -19,7 +19,14 @@ exports.createPost = (req, res, next) => {
   const title = req.body.title;
   const content = req.body.content;
   // create post in db
-  res
-    .status(201)
-    .json({ message: 'Post created successfully!', post: { title, content } });
+  res.status(201).json({
+    message: 'Post created successfully!',
+    post: {
+      title,
+      content,
+      _id: new Date().toISOString(),
+      creator: { name: 'Jack' },
+      createdAt: new Date(),
+    },
+  });
 };
