eeb2a3275dd45fafcd01f3f94e3fdf8e009a1e32
SOunit <workbookwork7@gmail.com>
2021-06-09 07:50:00 +0900

    connect user and post in db

--- 25_rest_api/node/controllers/feed.js
+++ 25_rest_api/node/controllers/feed.js
@@ -4,6 +4,7 @@ const path = require('path');
 const { validationResult } = require('express-validator/check');
 
 const Post = require('../models/post');
+const User = require('../models/user');
 
 exports.getPosts = (req, res, next) => {
   const currentPage = req.query.page || 1;
@@ -51,20 +52,31 @@ exports.createPost = (req, res, next) => {
   const title = req.body.title;
   const content = req.body.content;
 
+  let creator;
+
   // create post in db
   const post = new Post({
     title,
     content,
     imageUrl,
-    creator: { name: 'Jack' },
+    creator: req.userId,
   });
   post
     .save()
     .then((result) => {
-      console.log(result);
-      res
-        .status(201)
-        .json({ message: 'Post created successfully!', post: result });
+      return User.findById(req.userId);
+    })
+    .then((user) => {
+      creator = user;
+      user.posts.push(post);
+      return user.save();
+    })
+    .then((result) => {
+      res.status(201).json({
+        message: 'Post created successfully!',
+        post,
+        creator: { _id: creator._id, name: creator.name },
+      });
     })
     .catch((err) => {
       if (!err.statusCode) {
