7edec66ca31f2e21e3f10bed64532f9421ff8b90
SOunit <workbookwork7@gmail.com>
2021-06-08 22:40:16 +0900

    creating json web token when login

--- 25_rest_api/node/controllers/auth.js
+++ 25_rest_api/node/controllers/auth.js
@@ -1,6 +1,9 @@
 const User = require('../models/user');
 const { validationResult } = require('express-validator/check');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const envs = require('../envs');
 
 exports.signup = (req, res, next) => {
   const errors = validationResult(req);
@@ -27,6 +30,48 @@ exports.signup = (req, res, next) => {
     })
     .then((result) => {
       res.status(201).json({ message: 'User created', userId: result._id });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      // async code need next() to throw error
+      next(err);
+    });
+};
+
+exports.login = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  let loadedUser;
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        const error = new Error('A user with this email cound not be found.');
+        error.statusCode = 401;
+        throw error;
+      }
+      loadedUser = user;
+      return bcrypt.compare(password, user.password);
+    })
+    .then((isEqual) => {
+      if (!isEqual) {
+        const error = new Error('Wrong password!');
+        error.statusCode = 401;
+        throw error;
+      }
+      const token = jwt.sign(
+        {
+          email: loadedUser.email,
+          userId: loadedUser._id.toString(),
+        },
+        envs.USER_TOKEN_SECRET_KEY,
+        {
+          expiresIn: '1h',
+        }
+      );
+
+      res.status(200).json({ token, userId: loadedUser._id });
     })
     .catch((err) => {
       if (!err.statusCode) {
