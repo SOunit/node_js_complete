191963e810d8466f84bd928af4adc2696cb72a63
SOunit <workbookwork7@gmail.com>
2021-06-09 08:07:34 +0900

    delete post in user when post deleted

--- 25_rest_api/node/controllers/feed.js
+++ 25_rest_api/node/controllers/feed.js
@@ -193,7 +193,14 @@ exports.deletePost = (req, res, next) => {
       return Post.findByIdAndDelete(postId);
     })
     .then((result) => {
-      console.log(result);
+      return User.findById(req.userId);
+    })
+    .then((user) => {
+      // delete post from user posts
+      user.posts.pull(postId);
+      return user.save();
+    })
+    .then((result) => {
       res.status(200).json({ message: 'Deleted post.' });
     })
     .catch((err) => {
