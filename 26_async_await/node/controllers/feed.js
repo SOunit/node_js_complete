1098abb6acbea28657fccad0e43c2a3e107d32f2
SOunit <workbookwork7@gmail.com>
2021-06-09 13:19:49 +0900

    change then-catch to async-await

--- 26_async_await/node/controllers/feed.js
+++ 26_async_await/node/controllers/feed.js
@@ -6,32 +6,28 @@ const { validationResult } = require('express-validator/check');
 const Post = require('../models/post');
 const User = require('../models/user');
 
-exports.getPosts = (req, res, next) => {
+exports.getPosts = async (req, res, next) => {
   const currentPage = req.query.page || 1;
   const perPage = 2;
   let totalItems;
-  Post.find()
-    .countDocuments()
-    .then((count) => {
-      totalItems = count;
-      return Post.find()
-        .skip((currentPage - 1) * perPage)
-        .limit(perPage);
-    })
-    .then((posts) => {
-      res.status(200).json({
-        message: 'Fetched posts successfully.',
-        posts,
-        totalItems,
-      });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      // async code need next() to throw error
-      next(err);
+  try {
+    const count = await Post.find().countDocuments();
+    totalItems = count;
+    const posts = await Post.find()
+      .skip((currentPage - 1) * perPage)
+      .limit(perPage);
+    res.status(200).json({
+      message: 'Fetched posts successfully.',
+      posts,
+      totalItems,
     });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    // async code need next() to throw error
+    next(err);
+  }
 };
 
 exports.createPost = (req, res, next) => {
