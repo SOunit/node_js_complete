09479dac857ecb9a045bf076b146b5028ad90be3
SOunit <workbookwork7@gmail.com>
2021-08-21 08:58:43 -0700

    fix fetching posts error

--- 28_graphql/node/graphql/resolvers.js
+++ 28_graphql/node/graphql/resolvers.js
@@ -130,12 +130,13 @@ module.exports = {
     const posts = await Post.find().sort({ createdAt: -1 }).populate('creator');
     return {
       posts: posts.map((p) => {
-        return {
-          ...p,
+        const returnValue = {
+          ...p._doc,
           _id: p._id.toString(),
           createdAt: p.createdAt.toISOString(),
           updatedAt: p.updatedAt.toISOString(),
         };
+        return returnValue;
       }),
       totalPosts,
     };
