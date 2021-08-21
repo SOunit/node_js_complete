b3949bb7187fbed2f1e90dd6938770244394a7ab
SOunit <workbookwork7@gmail.com>
2021-08-21 16:14:33 -0700

    add update post in graphql

--- 28_01_graphql/node/graphql/resolvers.js
+++ 28_01_graphql/node/graphql/resolvers.js
@@ -151,11 +151,11 @@ module.exports = {
     };
   },
   post: async ({ id }, req) => {
-    // if (!req.isAuth) {
-    //   const error = new Error('Not authenticated!');
-    //   error.code = 401;
-    //   throw error;
-    // }
+    if (!req.isAuth) {
+      const error = new Error('Not authenticated!');
+      error.code = 401;
+      throw error;
+    }
 
     const post = await Post.findById(id).populate('creator');
     if (!post) {
@@ -166,6 +166,39 @@ module.exports = {
 
     return {
       ...post._doc,
+      _id: post._id.toString(),
+      createdAt: post.createdAt.toISOString(),
+      updatedAt: post.updatedAt.toISOString(),
+    };
+  },
+  updatePost: async ({ id, postInput }, req) => {
+    if (!req.isAuth) {
+      const error = new Error('Not authenticated!');
+      error.code = 401;
+      throw error;
+    }
+
+    const post = await Post.findById(id).populate('creator');
+    if (!post) {
+      const error = new Error('Post not found');
+      error.code = 404;
+      throw error;
+    }
+
+    if (post.creator._id.toString() !== req.userId.toString()) {
+      const error = new Error('Not authorized!');
+      error.code = 404;
+      throw error;
+    }
+
+    post.title = postInput.title;
+    post.content = postInput.content;
+    if (postInput.imageUrl !== 'undefined') {
+      post.imageUrl = postInput.imageUrl;
+    }
+    const updatedPost = await post.save();
+    return {
+      ...updatedPost._doc,
       _id: post._id.toString(),
       createdAt: post.createdAt.toISOString(),
       updatedAt: post.updatedAt.toISOString(),
