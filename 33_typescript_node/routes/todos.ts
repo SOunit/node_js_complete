addef4b04a23a0df43e2aceb7c3560c5a6debcf7
SOunit <workbookwork7@gmail.com>
2021-08-22 16:55:15 -0700

    add push to node with typescript

--- 33_typescript_node/routes/todos.ts
+++ 33_typescript_node/routes/todos.ts
@@ -7,19 +7,35 @@ import { Todo } from '../models/todo';
 
 const router = Router();
 
-const todos: Todo[] = [];
+let todos: Todo[] = [];
 
 router.get('/', (req, res, next) => {
   res.status(200).json({ todos });
 });
 
-router.post('/post', (req, res, next) => {
+router.post('/todo', (req, res, next) => {
   const newTodo: Todo = {
     id: Math.random().toString(),
     text: req.body.text,
   };
 
   todos.push(newTodo);
+  res.status(201).json({ message: 'Added todo', todo: newTodo, todos });
+});
+
+router.put('/todo/:todoId', (req, res, next) => {
+  const tid = req.params.todoId;
+  const todoIndex = todos.findIndex((todoItem) => todoItem.id === tid);
+  if (todoIndex >= 0) {
+    todos[todoIndex] = { id: todos[todoIndex].id, text: req.body.text };
+    return res.status(200).json({ message: 'Updated todo', todos });
+  }
+  res.status(404).json({ message: 'Could not find todo fot this id.' });
+});
+
+router.delete('/todos/:todoId', (req, res, next) => {
+  todos = todos.filter((todoItem) => todoItem.id !== req.params.todoId);
+  res.status(200).json({ message: 'Deleted todo', todos });
 });
 
 export default router;
