a3fdb6ce5bf6ffbc91cdaf554f2e2b82624b4d8a
SOunit <workbookwork7@gmail.com>
2021-08-22 15:11:12 -0700

    add post todo request

--- 33_typescript_node/routes/todos.ts
+++ 33_typescript_node/routes/todos.ts
@@ -1,11 +1,25 @@
 import { Router } from 'express';
 
+// {} is for name export
+// name export is like below
+// export interface Todo {}
+import { Todo } from '../models/todo';
+
 const router = Router();
 
-const todos: string[] = [];
+const todos: Todo[] = [];
 
 router.get('/', (req, res, next) => {
   res.status(200).json({ todos });
+});
+
+router.post('/post', (req, res, next) => {
+  const newTodo: Todo = {
+    id: Math.random().toString(),
+    text: req.body.text,
+  };
+
+  todos.push(newTodo);
 });
 
 export default router;
